6003b5eb57f2921485ea9fa84cdaa19a8aee8ca7
aeauseth <aeauseth@users.noreply.github.com>
2025-04-04 13:42:09 -0700

    holding testing

--- module/combatant.mjs
+++ module/combatant.mjs
@@ -20,10 +20,13 @@ export class HeroSystem6eCombatant extends Combatant {
         const index = Math.min(Math.max(actor.system.characteristics.spd?.value || 0, 1), 12);
         const phases = foundry.utils.deepClone(HeroSystem6eCombatant.Speed2Segments[index]);
 
-        // if (this.flags.delayUntil?.segment && options.combatCurrent) {
-        //     phases[phases.findIndex((p) => p == this.flags.delayUntil.segment)] =
-        //         parseInt(this.flags.delayUntil.segment) + 1;
-        // }
+        if (this.flags.holdingAnAction) {
+            var _idx = phases.indexOf(this.flags.holdingAnAction.initSegment);
+
+            if (_idx !== -1) {
+                phases[_idx] = this.flags.holdingAnAction.targetSegment;
+            }
+        }
 
         let _hasPhase = phases.includes(segmentNumber);
         return _hasPhase;
