73ef10aab92aa1fa70f9afa08345c41acc3c9e15
aeauseth <aeauseth@users.noreply.github.com>
2025-09-12 15:34:19 -0700

    item.system.active

--- module/item/item.mjs
+++ module/item/item.mjs
@@ -2554,7 +2554,6 @@ export class HeroSystem6eItem extends Item {
 
     configureAttackParameters() {
         console.error("depricated configureAttackParameters");
-        debugger;
 
         const maneuver = ["maneuver", "martialart"].includes(this.type);
 
